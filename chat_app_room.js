f8aa986cad3eabed78cced3151571f4b7279c2cf
awesomeness674 <86088545+awesomeness674@users.noreply.github.com>
2022-01-15 18:29:12 -0800

    Add files via upload

--- chat_app_room.js
+++ chat_app_room.js
@@ -20,7 +20,7 @@ function addRoom()
           room_name = document.getElementById("room_name").value;
           firebase.database().ref("/").child(room_name).update({ purpose : "adding room name" });
     localStorage.setItem("room_name" , room_name);
-    window.location = "kwitter_page.html"
+    window.location = "chat_app_page.html"
     
       }
 function getData()
